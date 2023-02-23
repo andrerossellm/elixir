cc13b0784e679f16becf64b1472104dc34d15994
arm <a01736035@tec.mx>
2023-02-23 11:05:20 -0600

    Completed one spare test case

--- bowling/lib/bowling.ex
+++ bowling/lib/bowling.ex
@@ -1,8 +1,5 @@
 defmodule Bowling do
   def score([]), do: 0 #caso base
-  def score([frame|rest]) do
-    first = hd(frame)
-    second = hd(tl(frame))
-    first + second + score(rest)
+  def score([frame|rest]) when hd(frame)+ hd(tl(frame))==10, do: 10 + hd(hd(rest)) + score(rest)
+  def score([frame|rest]), do: hd(frame)+ hd(tl(frame))+ score(rest)
   end
-end
--- bowling/test/bowling_test.exs
+++ bowling/test/bowling_test.exs
@@ -5,7 +5,12 @@ defmodule BowlingTest do
     assert Bowling.score(game)==0
   end
   test "'all ones' game" do
-  game = List.duplicate([1, 1], 9) ++ [[1, 1, nil]]
-  assert Bowling.score(game) == 20
+    game = List.duplicate([1, 1], 9) ++ [[1, 1, nil]]
+    assert Bowling.score(game) == 20
+  end
+  test "one spare" do
+    game = [[5,5],[3,0]] ++ List.duplicate([0, 0], 7)
+    ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 16
   end
 end
